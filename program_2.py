7c5c2b1f4c65af1967298318477ec4c2bb5aa61b
samgaines0908-dev <sam.gaines0908@gmail.com>
2026-02-19 12:29:23 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -8,13 +8,16 @@
 #Author: Sam Gaines
 #Date: 2/19/2026
 #Title: Math Quiz
-
+import random
 def math_quiz():
-    num1= 247
-    num2= 129
+    #chooses a number
+    num1= random.radiant(1,1000)
+    num2= random.radiant(1,1000)
+    #asks user to add the two numbers 
     print(f"What is {num1}+{num2}?")
     user_answer= int(input("Please enter your answer"))
     correct_answer= num1 + num2
+    #Congratulates user if correct, corrects user if wrong 
     if user_answer == correct_answer:
         print("Congratulations! You got the correct answer!")
     else:
